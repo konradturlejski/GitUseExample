5824a62aaa7fa7e25b3dc6e0dd016a45e9eaaf5f
Konrad Turlejski <konrad.turlejski1@gmail.com>
2018-12-30 21:12:40 +0100

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Konrad Turlejski on 30/12/2018.
+//  Copyright © 2018 Konrad Turlejski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
