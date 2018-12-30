54b658376e0d9dfff53224a644ddb924f6d5aa80
Konrad Turlejski <konrad.turlejski1@gmail.com>
2018-12-30 22:48:54 +0100

    Add new map for itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Konrad Turlejski on 30/12/2018.
+//  Copyright © 2018 Konrad Turlejski. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
